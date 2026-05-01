6fabf2cd93387503c4e8ef08a637a88c2f0dce24
ShekharSinha7325 <shekharghura824@gmail.com>
2026-05-01 17:45:59 +0530

    new Updated

--- first.py
+++ first.py
@@ -11,3 +11,38 @@ num2= 10
 sum1= num1+num2
 print(f"{num1} and {num2} is equal to {sum1}")
 
+
+#I/O Operation
+# input  return string value
+#num1= int(input("enter Your Number:"))
+#num2= int(input("enter Your Number:"))
+#res= num1 + num2
+#print(res)
+
+
+# type casting --> Which is coverted force fully
+# type conversion --> Which is converted automatically
+
+num = 100
+name = "Rahul"
+#print(num + name)
+print(type(name))
+
+# Type Casting
+#var1 = float(1000)
+# var1 = (1000)
+# print(type(var1))
+# print(var1)
+
+# num1 = "50"
+# num2 = "20"
+# res= num1+num2
+# print(res)
+# print(type(res))
+
+num1 = 50
+num2 = 20
+print(type(num1), type(num2))
+res= str(num1)+str(num2)
+print(res)
+print(type(res))
