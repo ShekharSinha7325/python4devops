50d28ad8a3910cf2d9959642d032adddb587c766
ShekharSinha7325 <shekharghura824@gmail.com>
2026-05-05 10:33:43 +0530

    New update

---
+++ 5-may.py
@@ -0,0 +1,50 @@
+# 5. Membership Operator
+
+# str1 = "this is python for devop 109"
+# #find = "this" # it is in the sequence
+# find = "This" # it's not in the sequence
+# print( find in str1) # it is cheque letter,word in the given string
+
+# indexing in python
+# num1 =  "4567"
+# #print("4" in num1)
+# print("4" not in num1)
+# #print(num1[3])
+
+
+# email = "iqindia123@gmail.com"
+# find = "@gmail.com"
+# #print(find in email)
+# if find in email:
+#     print("This is valid email")
+# else:
+#     print("invaild email")
+
+
+
+# age compare
+# aman_age = 20
+# abhishek_age = 30
+# if aman_age > abhishek_age:
+#     print("Yes aman age is more than abhishek age")
+# else:
+#     print("no aman age is not more than abhishek age")
+
+
+
+
+#user is eligible for voting or not
+
+min_age = 18
+nationality = "IN"
+user_age = int(input("Enter your age : "))
+user_id = input("Enter your nationality(ex. :IN) : ")
+if user_age >= min_age and user_id == nationality:
+    print("User is eligible for voting")
+else:
+    print("User is not eligible for voting")
+
+
+
+
+
