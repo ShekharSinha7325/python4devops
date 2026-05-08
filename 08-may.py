7f623536d1c2a106b991eaff09c4998866b839cd
ShekharSinha7325 <shekharghura824@gmail.com>
2026-05-08 11:38:54 +0530

    08-may python practice question

---
+++ 08-may.py
@@ -0,0 +1,48 @@
+# Loops in python
+# 1.for loop : range
+# 2. while loop : condition based
+
+#1. for loop
+# Syntax :-  for variable in sequence:
+#             code#
+ # range(start -0, stop-1, step : ) it is always work with integer value
+
+#print("Aman\n " *10)
+
+# for i in range(10):
+#     print(i, end=" ")
+# print("----------------\n")
+# for i in range(5,10,1):
+#     print(i, end=" ")
+#     print("python")
+# for i in range (1,20):
+#     if i == 10:
+#         break
+#     print(i)
+# for i in range (1,20):
+#     if i == 10:
+#         continue
+#     print(i)
+
+# for i in range (1,20):
+#     if i %2 ==0:
+#         print(i, end=" ")  #end=" " use the change the formating vertical to horizontal
+
+# for i in range (1, 22):
+#     if i%2 != 0:
+#         print(f"odd : {i}")
+#     else:
+#         print(f"Even : {i}")
+
+# s = 0
+# for i in range (1,5):
+#     s+= i
+# print(s)
+
+
+# for i in range(10,11,-1):
+#     print(i)
+
+
+
+
---
+++ practice__question.py
@@ -0,0 +1,55 @@
+#1. wap to takes start_point and end_point from user input and print all number divisble by 2 and 3
+start_point = int(input("Enter start Point : "))
+end_point = int(input("Enter end Point : "))
+
+for i in range(start_point,end_point):
+    if i%2 == 0:
+        print(f"Number Divisble by 2 : {i}")
+    if i %3 == 0:
+        print(f"Number divisble by 3 : {i}")
+
+
+
+#Another Way if you want print seprate divisble by 2 and divisble by 3
+start_point = int(input("Enter start Point : "))
+end_point = int(input("Enter end Point : "))
+
+print("Numbers Divisible by 2")
+
+for i in range(start_point, end_point + 1):
+    if i % 2 == 0:
+        print(i)
+
+print("Numbers Divisible by 3")
+
+for i in range(start_point, end_point + 1):
+    if i % 3 == 0:
+        print(i)
+   
+
+
+#2. wap to take a number from user input and print formated table
+# formate
+# 3x1=3
+# 3x2=6
+# ....
+# 3x10 = 30
+
+Num = int(input("Enter a number : "))
+for i in range(1,11):
+    n1 = Num*i
+    print(f"{Num} X {i} : {n1}")
+
+
+
+#3. wap to take a number from user input and print  reverse formated table
+# formate
+# 3x10=30
+# 3x9=27
+# ....
+# 3x1 = 3
+
+Num = int(input("Enter a number : "))
+for i in range(10,0,-1):
+    n1 = Num*i
+    print(f"{Num} X {i} : {n1}")
